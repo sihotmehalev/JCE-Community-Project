86c9f3c29d28d1b013eeb2a4daf8633717a8cae5
Nadav Robinson <nadav832002@gmail.com>
2025-04-24 13:07:52 +0300

    modified firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: console.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: console.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: console.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: console.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: console.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: console.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-QQQ4EW03S6"
 };
 
