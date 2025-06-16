b79312d5571178030ebd11cc0b9e0b1c81aefb84
Yuvalcohen22 <yuvalcohen1701@gmail.com>
2025-06-16 23:52:42 +0300

    feat: Add EventSlider to Layout for conditional rendering on specific pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,25 @@
 import Navbar from './Navbar';
+import { EventSlider } from "./EventSlider/EventSlider";
+import { useLocation } from 'react-router-dom';
 
 export default function Layout({ children },) {
 
+    const location = useLocation();
+    const showEventsPages = ['/requester-dashboard', '/volunteer-dashboard'];
+    const shouldShowEvents = showEventsPages.includes(location.pathname);
+
     return (
-        <div className="min-h-screen bg-gradient-to-br from-red-200 via-orange-200 to-red-300">
+        <div className="min-h-screen flex flex-col bg-gradient-to-br from-red-200 via-orange-200 to-red-300">
             <Navbar />
 
             {/* Main Content */}
             <main className="container mx-auto px-4 py-4 items-center justify-center">
                 {children}
             </main>
+
+            { shouldShowEvents && (
+                <EventSlider />
+            )}
 
             <footer className="bg-orange-100/30 backdrop-blur-sm py-4 mt-auto">
                 <div className="container mx-auto px-4 text-center text-orange-800">
