384ce201ed3e5dc40fef4ee1dd326639773c6487
Nir Ben Boaz <nir40000@gmail.com>
2025-07-06 09:52:03 +0300

    added interested users in events chart to statistics, admin can now move accross time axis in the chart, (look at the past or future) by click הקודם הבא buttons

--- src/components/analytics/AnalyticsTab.jsx
+++ src/components/analytics/AnalyticsTab.jsx
@@ -5,14 +5,7 @@ export const AdminAnalyticsTab = () => {
     console.log('AdminAnalyticsTab rendered');
     return (
         <div >
-            <h3 className="font-semibold mb-4 text-orange-700">
-            סטטיסטיקה
-            </h3>
-            <div className="flex flex-col w-full space-y-4">
-                    <div className="bg-white rounded-lg shadow p-6 text-center">
-                        <UnifiedAnalyticsBarChart />
-                    </div>
-            </div>
+                   <UnifiedAnalyticsBarChart /> 
         </div>
     );
 };
